153026ef27e41a50346311625dbcb1be44b63c9e
mohangrg2018 <mohangrg2030@gmail.com>
2025-07-10 12:55:08 +0545

    navbar component created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import Display from "./components/Display";
+import Player from "./components/Player";
 import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <div className="w-screen h-screen bg-black">
-      <Sidebar />
+      <div className="h-[90%] flex">
+        <Sidebar />
+        <Display />
+      </div>
+      <Player />
     </div>
   );
 };
